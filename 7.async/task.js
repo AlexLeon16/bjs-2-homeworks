ce31b44ce94511df8c89ba7c89ebd2ccb992ef7e
AlexLeon16 <aleksandravelichko@mail.ru>
2026-06-11 14:30:23 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,74 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+
+    const hasSameTime = this.alarmCollection.some(function (alarm) {
+      return alarm.time === time;
+    });
+
+    if (hasSameTime) {
+      console.warn("Уже присутствует звонок на это же время");
+    }
+
+    this.alarmCollection.push({
+      time,
+      callback,
+      canCall: true,
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(function (alarm) {
+      return alarm.time !== time;
+    });
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+
+    return `${hours}:${minutes}`;
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(function (alarm) {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(function (alarm) {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
